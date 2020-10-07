f3e84f0248f40ef846511fc0443a58e163894895
Juan Ruitiña <git@ruitina.com>
2020-10-07 20:29:20 +0200

    Check if process is running

--- calcifair.py
+++ calcifair.py
@@ -21,6 +21,29 @@ from ltr559 import LTR559
 import ST7789
 
 from setproctitle import setproctitle
+import psutil
+
+
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    https://thispointer.com/python-check-if-a-process-is-running-by-name-and-find-its-process-id-pid/
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
+
+
+# Check if any chrome process was running or not.
+if checkIfProcessRunning('calcifair-main'):
+    print("🔥 Calcifer is awake already")
+    exit()
 
 setproctitle('calcifair-main')
 
